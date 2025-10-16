4da2aa27b04e27492e7ecbe58e1b96c6455f73e2
Lucas <114384142+c0dag@users.noreply.github.com>
2025-10-16 23:32:30 +0000

    models

--- atividade-te/app/Models/Categoria.php
+++ atividade-te/app/Models/Categoria.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Categoria extends Model
+class Produto extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nome', 'preco', 'categoria_id'];
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class);
+    }
 }
--- atividade-te/app/Models/Produto.php
+++ atividade-te/app/Models/Produto.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Produto extends Model
+class Categoria extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nome'];
+
+    public function produtos()
+    {
+        return $this->hasMany(Produto::class);
+    }
 }
